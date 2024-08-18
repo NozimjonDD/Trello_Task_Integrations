10e96af44444024cd07ea2a32aacca889d00794f
Ubaydulloh <ubaydulloh1000@gmail.com>
2024-08-18 18:09:00 +0500

    REF: football app models registered to admin

--- apps/football/admin.py
+++ apps/football/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.League)
+class LeagueAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Season)
+class SeasonAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Round)
+class RoundAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.MatchState)
+class MatchStateAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Match)
+class MatchAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Club)
+class ClubAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Position)
+class PositionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Player)
+class PlayerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.ClubPlayer)
+class ClubPlayerAdmin(admin.ModelAdmin):
+    pass
