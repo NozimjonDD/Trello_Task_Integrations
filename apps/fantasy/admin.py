56c17173a5c47a4973e697c8f2e51e88a2405279
Ubaydulloh <ubaydulloh1000@gmail.com>
2024-08-20 14:57:06 +0500

    REF: squad, squad player models added

--- apps/fantasy/admin.py
+++ apps/fantasy/admin.py
@@ -5,6 +5,13 @@ from django.utils.translation import gettext_lazy as _
 from . import models
 
 
+class FormationPositionInline(admin.StackedInline):
+    extra = 0
+    model = models.FormationPosition
+    autocomplete_fields = ("position",)
+    exclude = ("is_deleted", "deleted_at",)
+
+
 @admin.register(models.Formation)
 class FormationAdmin(admin.ModelAdmin):
     list_display = ("title", "scheme", "ordering", "id",)
@@ -12,6 +19,8 @@ class FormationAdmin(admin.ModelAdmin):
     search_fields = ("title", "scheme", "id",)
     list_editable = ("ordering",)
     ordering = ("ordering",)
+    exclude = ("is_deleted", "deleted_at",)
+    inlines = (FormationPositionInline,)
 
 
 @admin.register(models.Team)
@@ -21,6 +30,16 @@ class TeamAdmin(admin.ModelAdmin):
 
 @admin.register(models.TeamPlayer)
 class TeamPlayerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Squad)
+class SquadAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.SquadPlayer)
+class SquadPlayerAdmin(admin.ModelAdmin):
     pass
 
 
--- apps/fantasy/models.py
+++ apps/fantasy/models.py
@@ -20,6 +20,32 @@ class Formation(BaseModel):
         return self.title
 
 
+class FormationPosition(BaseModel):
+    class Meta:
+        db_table = "formation_position"
+        verbose_name = _("Formation position")
+        verbose_name_plural = _("Formation positions")
+
+    formation = models.ForeignKey(
+        to="fantasy.Formation",
+        on_delete=models.CASCADE,
+        related_name="positions",
+        verbose_name=_("Formation")
+    )
+    position = models.ForeignKey(
+        to="football.Position",
+        on_delete=models.CASCADE,
+        related_name="+",
+        verbose_name=_("Position"),
+    )
+    index = models.IntegerField(verbose_name=_("Index"), default=0)
+    coordinate_x = models.IntegerField(verbose_name=_("Coordinate X"), default=0)
+    coordinate_y = models.IntegerField(verbose_name=_("Coordinate Y"), default=0)
+
+    def __str__(self):
+        return f"{self.formation} - [{self.coordinate_x}:{self.coordinate_y}]"
+
+
 class Team(BaseModel):
     class Meta:
         db_table = "fantasy_team"
@@ -27,7 +53,7 @@ class Team(BaseModel):
     user = models.OneToOneField(to="users.User", on_delete=models.CASCADE, related_name="team")
     name = models.CharField(verbose_name=_("Name"), max_length=255, null=True)
     status = models.CharField(choices=TeamStatusChoices.choices, default=TeamStatusChoices.INACTIVE, max_length=100)
-    formation = models.ForeignKey(to="fantasy.Formation", on_delete=models.SET_NULL, null=True)
+    formation = models.ForeignKey(to="fantasy.Formation", on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -45,6 +71,49 @@ class TeamPlayer(BaseModel):
 
     def __str__(self):
         return f"{self.team} - {self.player}"
+
+
+class Squad(BaseModel):
+    class Meta:
+        db_table = "fantasy_squad"
+
+    team = models.ForeignKey(to="fantasy.Team", on_delete=models.CASCADE, related_name="squads", verbose_name=_("Team"))
+    round = models.ForeignKey(
+        to="football.Round", on_delete=models.CASCADE, related_name="+", verbose_name=_("Round")
+    )
+    formation = models.ForeignKey(
+        to="fantasy.Formation", on_delete=models.SET_NULL, null=True, verbose_name=_("Formation"), related_name="+"
+    )
+
+    def __str__(self):
+        return f"{self.team} - {self.round}"
+
+
+class SquadPlayer(BaseModel):
+    class Meta:
+        db_table = "fantasy_squad_player"
+
+    squad = models.ForeignKey(
+        to="fantasy.Squad",
+        on_delete=models.CASCADE,
+        related_name="players",
+        verbose_name=_("Squad"),
+    )
+    player = models.ForeignKey(
+        to="fantasy.TeamPlayer",
+        on_delete=models.CASCADE,
+        related_name="+",
+        verbose_name=_("Team player")
+    )
+    position = models.ForeignKey(
+        to="fantasy.FormationPosition",
+        on_delete=models.CASCADE,
+        related_name="+",
+        verbose_name=_("Formation position"),
+    )
+
+    def __str__(self):
+        return f"{self.player} - {self.position}"
 
 
 class FantasyLeague(BaseModel):
