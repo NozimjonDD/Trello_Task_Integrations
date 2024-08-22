817cdfae2f75dd0557bf3a1b769b2244f0a95369
NozimjonDD <nozimfromandijan@gmail.com>
2024-08-22 17:47:30 +0500

    PremierLeagueStatusByPlayer added

--- api/v1/site/football/views.py
+++ api/v1/site/football/views.py
@@ -1,11 +1,11 @@
-from rest_framework.generics import (
-    CreateAPIView,
-    RetrieveAPIView,
-    UpdateAPIView,
-    DestroyAPIView,
-    ListAPIView,
-    RetrieveUpdateAPIView,
-)
-from rest_framework.permissions import AllowAny
+from rest_framework import views
 from rest_framework.response import Response
-from rest_framework.views import APIView
+
+from apps.football.utils import update_premierleague_status_by_players
+
+
+class AStat(views.APIView):
+    def post(self, request, *args, **kwargs):
+        count = update_premierleague_status_by_players()
+
+        return Response({"message": "class A", "count":count})
