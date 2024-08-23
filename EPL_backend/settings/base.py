312375fb2400bcc0ae50a9577f4f307c2723cec9
Ubaydulloh <ubaydulloh1000@gmail.com>
2024-08-23 19:32:00 +0500

    FEAT: custom exception handler added

--- EPL_backend/settings/base.py
+++ EPL_backend/settings/base.py
@@ -141,6 +141,7 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 10,
+    "EXCEPTION_HANDLER": "EPL_backend.utils.custom_exception_handler"  # noqa
 }
 
 LANGUAGE_CODE = 'uz'
