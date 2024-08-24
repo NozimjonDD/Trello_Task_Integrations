99509acaf439736704815ed381a73a3a9f1c20a0
Ubaydulloh <ubaydulloh1000@gmail.com>
2024-08-24 18:17:02 +0500

    REF: admin player list show premier league players only

--- apps/football/admin.py
+++ apps/football/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+
 from django.utils.html import format_html
 
 from django.utils.translation import gettext_lazy as _
@@ -135,6 +137,7 @@ class PlayerAdmin(admin.ModelAdmin):
 
     def get_queryset(self, request):
         qs = super().get_queryset(request).select_related("club", "position", )
+        qs = qs.filter(club__league__remote_id=settings.PREMIER_LEAGUE_ID)
         return qs
 
 
