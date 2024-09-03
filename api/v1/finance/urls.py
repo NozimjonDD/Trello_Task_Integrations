c8f985bb660b99c1db98199198d591b7ddc7fa39
NozimjonDD <nozimfromandijan@gmail.com>
2024-09-03 19:07:38 +0500

    calculate_total_price

--- api/v1/finance/urls.py
+++ api/v1/finance/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path("tariff-list/", views.TariffListAPIView.as_view(), name="tariff-list"),
 
-    #
+    # Subscription
     path("subscription/", views.SubscriptionAPIView.as_view(), name="subscription-join"),
 
 ]
